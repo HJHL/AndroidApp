1343530c9c0a6a6cd13d028dcd6d2899c9ebcba5
iljh <46100114+iljh@users.noreply.github.com>
2022-01-13 18:48:36 +0800

    feat: add change size api for MyGLSurfaceView

--- app/src/main/java/me/lijiahui/androidapp/widget/MyGLSurfaceView.kt
+++ app/src/main/java/me/lijiahui/androidapp/widget/MyGLSurfaceView.kt
@@ -21,6 +21,11 @@ class MyGLSurfaceView(context: Context, attributeSet: AttributeSet?) :
 
     val nativeRender: MyNativeRender
 
+    var ratioWidth = 0
+        private set
+    var ratioHeight = 0
+        private set
+
     init {
         // 使用 OpenGL ES 3.0 环境
         setEGLContextClientVersion(3)
@@ -29,6 +34,41 @@ class MyGLSurfaceView(context: Context, attributeSet: AttributeSet?) :
         setRenderer(this)
         // 设置渲染模式为持续渲染模式
         renderMode = RENDERMODE_CONTINUOUSLY
+    }
+
+    /**
+     * 修改 SurfaceView 大小
+     *
+     * @param width         修改后的宽
+     * @param height        修改后的高
+     * */
+    fun setAspectRatio(width: Int, height: Int) {
+        Log.d(TAG, "setAspectRatio old ${ratioWidth}x$ratioHeight -> new ${width}x$height")
+        if (width < 0 || height < 0) {
+            throw IllegalArgumentException("Size cannot be negative")
+        }
+        ratioWidth = width
+        ratioHeight = height
+        requestLayout()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        Log.d(
+            TAG,
+            "onMeasure input ${widthMeasureSpec}x$heightMeasureSpec ratio ${ratioWidth}x$ratioHeight"
+        )
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+        if (ratioWidth == 0 || ratioHeight == 0) {
+            setMeasuredDimension(width, height)
+        } else {
+            if (width < height * ratioWidth / ratioHeight) {
+                setMeasuredDimension(width, width * ratioHeight / ratioWidth)
+            } else {
+                setMeasuredDimension(height * ratioWidth / ratioHeight, height)
+            }
+        }
     }
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
