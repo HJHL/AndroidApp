5a7bad8f75e11ff473030feedf9901a4480c854e
Li Jiahui <HJHL@users.noreply.github.com>
2022-01-16 20:00:52 +0800

    feat: glsl greyscale

--- app/src/main/assets/custom_fragment_code.glsl
+++ app/src/main/assets/custom_fragment_code.glsl
@@ -9,6 +9,10 @@ uniform sampler2D texture0;
 
 out vec4 fragColor;
 
+const vec3 M = vec3(0.299, 0.587, 0.114);
+
 void main() {
-    fragColor = texture(texture0, texCoord);
+    vec4 mask = texture(texture0, texCoord);
+    float lum = dot(mask.rgb, M);
+    fragColor = vec4(vec3(lum), 1.0);
 }
