8d55630df966b41bec7421cf2f2c5494f5ed43a5
iljh <46100114+iljh@users.noreply.github.com>
2022-02-09 16:54:22 +0800

    feat: add Compose example

--- app/src/main/java/me/lijiahui/androidapp/demo/DemoActivity.kt
+++ app/src/main/java/me/lijiahui/androidapp/demo/DemoActivity.kt
@@ -1,11 +1,13 @@
 package me.lijiahui.androidapp.demo
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import me.lijiahui.androidapp.R
 import me.lijiahui.androidapp.databinding.ActivityDemoBinding
+import me.lijiahui.androidapp.demo.compose.ComposeActivity
 import me.lijiahui.androidapp.demo.exoplayer.ExoFragment
 import me.lijiahui.androidapp.demo.ffmpeg.FfmpegFragment
 import me.lijiahui.androidapp.demo.floating.FloatingFragment
@@ -32,6 +34,10 @@ class DemoActivity : AppCompatActivity() {
     }
 
     private fun translateTo(title: CharSequence, menuId: Int) {
+        if (menuId == R.id.menu_id_compose) {
+            startActivity(Intent(this, ComposeActivity::class.java))
+            finish()
+        }
         setTitle(title)
         when (menuId) {
             R.id.menu_id_exo -> {
